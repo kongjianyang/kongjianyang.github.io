21c86c0791a7586dc974509cf5b9dacce323b34d
Liang Tang <liangtang@pal-nat186-159-175.itap.purdue.edu>
2022-03-25 12:39:50 -0400

    updating site on Fri Mar 25 12:39:40 EDT 2022; 修改了deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 git add .
-update_time ="updating site on $(date)" 
+update_time="updating site on $(date)" # 不能有空格
 read -p  "请输入commit信息：" -t 30 commit_msg #等待30秒
 echo $commit_msg
 msg="$update_time; $commit_msg" 
