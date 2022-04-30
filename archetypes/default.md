d5c55386e1cfd08000c73ab304531f18ac8e47c5
Liang Tang <liangtang@pal-nat186-159-160.itap.purdue.edu>
2022-04-30 19:12:50 -0400

    updating site on Sat Apr 30 19:12:43 EDT 2022; add 加密

--- archetypes/default.md
+++ archetypes/default.md
@@ -1,6 +1,13 @@
 ---
 title: "{{ replace .Name "-" " " | title }}"
 date: {{ .Date }}
+author: KJY
+slug: ["some here"]
 draft: true
+categories:  
+  -[ "Note" ]
+tags:        
+  - article
 ---
 
+** insert things here **
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!bin/bash
 
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
 git add .
 update_time="updating site on $(date)" # 不能有空格
 read -p  "请输入commit信息：" -t 30 commit_msg #等待30秒
